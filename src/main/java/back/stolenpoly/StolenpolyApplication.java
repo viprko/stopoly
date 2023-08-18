53eebd5f7fa8d5b898c2cde77d2d55f1b3d310b2
viprko <pr.and.vi@gmail.com>
2023-08-18 08:59:34 +0300

    First commit

---
+++ src/main/java/back/stolenpoly/StolenpolyApplication.java
@@ -0,0 +1,12 @@
+package back.stolenpoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StolenpolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StolenpolyApplication.class, args);
+    }
+}
---
+++ src/test/java/back/stolenpoly/StolenpolyApplicationTests.java
@@ -0,0 +1,12 @@
+package back.stolenpoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StolenpolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
